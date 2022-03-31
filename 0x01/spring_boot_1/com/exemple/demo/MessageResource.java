29f85f96d0f7b63398a2d1b677b27c6f5f2faea5
Flavia Almeida <eufga1@gmail.com>
2022-03-31 18:24:47 -0300

    Msgs Spring boot

---
+++ 0x01/spring_boot_1/com/exemple/demo/MessageResource.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController 
+@RequestMapping(value="/messages")
+public class MessageResource {
+
+	  @GetMapping("/simpleMessageWelcome")
+	    public String simpleMessageWelcome() {
+	        return "BEM VINDO A AULA DE MICROSERVIÇO USANDO SPRING BOOT !!!";
+
+	    }
+	  
+	  @GetMapping("/login")
+	    public String login(String user, String password) {
+		  String resposta = "LOGIN EFETUADO COM SUCESSO!!!" ;
+		  String usuario = "Flavia";
+	       String senha = "77889955";
+
+		  if (user.isEmpty()|| password.isEmpty())
+	        	resposta = "USUÁRIO E SENHA NÃO INFORMADOS";
+	        
+	        if (user.length()>15 || password.length()>15)
+	        	resposta = "USUÁRIO E SENHA INVÁLIDOS";
+	        
+	        return resposta;
+	    }
+}
